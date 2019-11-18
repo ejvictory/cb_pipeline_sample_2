4277a718c278a312d24d5d3680758857416fe143
ejvictory <eunju@mdstec.com>
2019-11-18 17:32:42 +0900

    Update JenkinsPipeline.groovy
    6

--- JenkinsPipeline.groovy
+++ JenkinsPipeline.groovy
@@ -7,5 +7,26 @@ pipeline {
           bat 'dir'
         }
       }
+	  stage('Where do ls command execute?') {
+        steps {
+          echo 'Here is default.'
+          bat 'dir /A'
+          echo 'you can define where to execute command.'
+          echo 'For example, I want to view root directory'
+          bat 'dir /A D:'
+        }
+      }
+	  stage('Is there any other way?') { 
+        steps {
+          echo 'Maybe sh is independent'
+          bat 'cd D:'
+          bat 'chdir'
+          bat 'dir /A'
+          echo 'If you want to execute multiple commands in one sh.'
+          bat '''cd /
+          chdir /
+          bat dir /A'''
+        }
+      }
     }
   }
